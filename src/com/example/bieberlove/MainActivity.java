65b31d6a586385a68326d3d9de9370c8ef426cb1
stratigo <williamicdc@gmail.com>
2015-05-31 00:01:10 -0400

    added menu options

--- src/com/example/bieberlove/MainActivity.java
+++ src/com/example/bieberlove/MainActivity.java
@@ -1,45 +1,47 @@
 package com.example.bieberlove;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
-	
+
 	ListView listTimeline;
 	DbHelper dbHelper;
 	SQLiteDatabase db;
 	Cursor cursor;
-	TimelineAdapter adapter; 
-	
+	TimelineAdapter adapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		listTimeline = (ListView) findViewById(R.id.listTimeline);
-		
-		 // Connect to database
-	    dbHelper = new DbHelper(this);
-	    db = dbHelper.getReadableDatabase();
+
+		// Connect to database
+		dbHelper = new DbHelper(this);
+		db = dbHelper.getReadableDatabase();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		  // Get the data from the database
-	    cursor = db.query(DbHelper.TABLE, null, null, null, null, null,
-	        DbHelper.C_CREATED_AT + " DESC");         
+		// Get the data from the database
+		cursor = db.query(DbHelper.TABLE, null, null, null, null, null,
+				DbHelper.C_CREATED_AT + " DESC");
 
-	    // Create the adapter
-	    adapter = new TimelineAdapter(this, cursor); 
-	    listTimeline.setAdapter(adapter);  
+		// Create the adapter
+		adapter = new TimelineAdapter(this, cursor);
+		listTimeline.setAdapter(adapter);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
@@ -48,20 +50,26 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
 		return true;
 	}
 
+	// Called when an options item is clicked
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+		case R.id.itemServiceStart:
+			startService(new Intent(this, UpdaterService.class));
+			break;
+		case R.id.itemServiceStop:
+			stopService(new Intent(this, UpdaterService.class));
+			break;
+		case R.id.itemTimeline:
+			// startActivity(new Intent(this, TimelineActivity.class));
+			onRestart();
+			break;
 		}
-		return super.onOptionsItemSelected(item);
+		return true;
 	}
 }
